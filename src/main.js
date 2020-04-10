1a79c33be856ca50ea18bf618cae5381ed530d3f
Anisa <aagermanova@gmail.com>
2020-04-10 09:54:42 -0700

    Удаляет функцию дозагрузки

--- src/main.js
+++ src/main.js
@@ -19,30 +19,21 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const tasks = generateTasks(TASK_COUNT);
-
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-
-const loadMoreTasks = () => {
-  const prevTasksCount = showingTasksCount;
-  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-  tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
-};
-
-let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-
 const init = () => {
   const filters = generateFilters();
+  const tasks = generateTasks(TASK_COUNT);
+
   render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
   render(siteMainElement, createFilterTemplate(filters), `beforeend`);
   render(siteMainElement, createBoardTemplate(), `beforeend`);
 
   const boardElement = siteMainElement.querySelector(`.board`);
+  const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
   render(boardElement, createSortingTemplate(), `afterbegin`);
   render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
+
+  let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 
   tasks.slice(1, showingTasksCount)
   .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
@@ -52,11 +43,18 @@ const init = () => {
   const loadMoreButton = boardElement.querySelector(`.load-more`);
 
   loadMoreButton.addEventListener(`click`, () => {
-    loadMoreTasks();
+    const prevTasksCount = showingTasksCount;
+    showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+    tasks.slice(prevTasksCount, showingTasksCount)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+    if (showingTasksCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
   });
-  if (showingTasksCount >= tasks.length) {
-    loadMoreButton.remove();
-  }
+
 };
 
 init();
+
