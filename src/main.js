2548d0929e957a44b28cb29532da15de696839cc
Anisa <aagermanova@gmail.com>
2020-04-10 17:14:17 -0700

    Добавляет функцию addLoadMore

--- src/main.js
+++ src/main.js
@@ -9,14 +9,36 @@ import {generateFilters} from "./mock/filter.js";
 import {generateTasks} from "./mock/task.js";
 
 const TASK_COUNT = 22;
-const SHOWING_TASKS_COUNT_ON_START = 8;
-const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
+const TASKS_PER_PAGE = 8;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
+};
+
+const addLoadMore = (tasks, boardElement, taskListElement) => {
+  let showingTasksCount = TASKS_PER_PAGE;
+
+  tasks.slice(1, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+  render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, () => {
+    const prevTasksCount = showingTasksCount;
+    showingTasksCount = showingTasksCount + TASKS_PER_PAGE;
+
+    tasks.slice(prevTasksCount, showingTasksCount)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+    if (showingTasksCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
 };
 
 const init = () => {
@@ -33,26 +55,7 @@ const init = () => {
   render(boardElement, createSortingTemplate(), `afterbegin`);
   render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-  let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-
-  tasks.slice(1, showingTasksCount)
-  .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
-
-  render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
-
-  const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-  loadMoreButton.addEventListener(`click`, () => {
-    const prevTasksCount = showingTasksCount;
-    showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-    tasks.slice(prevTasksCount, showingTasksCount)
-      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
-
-    if (showingTasksCount >= tasks.length) {
-      loadMoreButton.remove();
-    }
-  });
+  addLoadMore(tasks, boardElement, taskListElement);
 
 };
 
