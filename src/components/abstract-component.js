302dc4cebd7cc64ef2d24929109c451213cbcfbc
Anisa <aagermanova@gmail.com>
2020-04-19 17:16:05 -0700

    Переносит одинаковую реализацию общих для всех компонентов методов `getElement`, `removeElement`

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,11 +1,26 @@
 // Проверка в конструкторе очень важна. Она позволит использовать абстрактный
 // класс только в качестве родительского класса и выбросит ошибку при попытке
 // выполнить `const c = new AbstractComponent()`.
-
+import {createElement} from "../utils.js";
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
       throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
     }
+
+    // Посмотрев на все компоненты выделяем одинаковые для всех методы. Ими оказываются `getElement`, `removeElement`. Так же проинициализируем в конструкторе свойство `_element`.
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
