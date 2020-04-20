73e521334a54b3b0532b4e12980932185e8dedf3
Anisa <aagermanova@gmail.com>
2020-04-19 17:23:51 -0700

    Унаследует компонент `no-tasks` от абстрактного компонента

--- src/components/no-tasks.js
+++ src/components/no-tasks.js
@@ -1,27 +1,15 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
 
-export default class NoTasks {
-  constructor() {
-    this._element = null;
-  }
+const ceateNoTasksTemplate = () => {
+  return (
+    `<p class="board__no-tasks">
+       Click «ADD NEW TASK» in menu to create your first task
+     </p>`
+  );
+};
 
+export default class NoTasks extends AbstractComponent {
   getTemplate() {
-    return (
-      `<p class="board__no-tasks">
-         Click «ADD NEW TASK» in menu to create your first task
-       </p>`
-    );
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+    return ceateNoTasksTemplate();
   }
 }
