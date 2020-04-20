a3df74d7490c0b4586c81cdf2ffbde985314ccd3
Anisa <aagermanova@gmail.com>
2020-04-19 17:30:32 -0700

    Унаследует компоненты `site-menu`, `sort`, `tasks`, `load-more` от абстрактного компонента

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,27 +1,18 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
 
-export default class Sort {
-  constructor() {
-    this._element = null;
-  }
+const createSortTemplate = () => {
+  return (
+    `<div class="board__filter-list">
+      <a href="#" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter">SORT BY DATE up</a>
+      <a href="#" class="board__filter">SORT BY DATE down</a>
+    </div>`
+  );
+};
 
+
+export default class Sort extends AbstractComponent {
   getTemplate() {
-    return `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
-  </div>`;
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+    return createSortTemplate();
   }
 }
