3724461121586523baa83d5998cedc2c2fb08082
Anisa <aagermanova@gmail.com>
2020-04-26 13:36:41 -0700

    Перенесёт в контроллер задачи логику переключения. WIP

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,9 +1,51 @@
 import TaskComponent from "../components/task.js";
 import TaskEditComponent from "../components/task-edit.js";
-import {render, replace, RenderPosition} from "../utils/render.js";
+import { render, replace, RenderPosition } from "../utils/render.js";
 
 export default class TaskController {
-  constructor(container) {}
+  // Конструктор должен принимать container — элемент, в который контроллер будет всё отрисовывать.
+  constructor(container) {
+    this._container = container;
 
-  render(task) {}
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  // Метод render должен принимать данные одной задачи. Также в него должен переехать код, который отвечает за отрисовку задачи, ее замену на форму редактирования и наоборот, а также установка связанных с этим обработчиков событий.
+  render(task) {
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToTask();
+    });
+
+    render(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+  }
+
+  _replaceEditToTask() {
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+    replace(this._taskComponent, this._taskEditComponent);
+  }
+
+  _replaceTaskToEdit() {
+    replace(this._taskEditComponent, this._taskComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
 }
