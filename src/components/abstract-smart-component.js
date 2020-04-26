e085f2b18f523d8c20042e58048203ae77386d63
Anisa <aagermanova@gmail.com>
2020-04-26 14:11:24 -0700

    Создаст умный абстрактный компонент. WIP

---
+++ src/components/abstract-smart-component.js
@@ -0,0 +1,21 @@
+import AbstractComponent from "./abstract-component.js";
+
+export default class AbstractSmartComponent extends AbstractComponent {
+  // абстрактный метод recoveryListeners, его нужно будет реализовать в наследнике. Его задача — восстанавливать обработчики событий после перерисовки
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
+  }
+
+  rerender() {
+    const oldElement = this.getElement();
+    const parent = oldElement.parentElement;
+
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, oldElement);
+
+    this.recoveryListeners();
+  }
+}
