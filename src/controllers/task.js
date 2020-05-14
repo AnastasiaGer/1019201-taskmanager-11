e995866ffc2d8cbbecc7b3c4b1d7492d7f68f015
Anisa <aagermanova@gmail.com>
2020-05-13 17:38:19 -0700

    Реализует обратную связь "покачивание головой" при неудавшихся сетевых запросах

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,6 +3,7 @@ import TaskEditComponent from "../components/task-edit.js";
 import TaskModel from "../models/task.js";
 import {render, replace, remove, RenderPosition} from "../utils/render.js";
 import {Color, DAYS} from "../const.js";
+const SHAKE_ANIMATION_TIMEOUT = 600;
 
 export const Mode = {
   ADDING: `adding`,
@@ -128,6 +129,16 @@ export default class TaskController {
     remove(this._taskEditComponent);
     remove(this._taskComponent);
     document.removeEventListener(`keydown`, this._onEscKeyDown);
+  }
+
+  shake() {
+    this._taskEditComponent.getElement().style.animation = `shake ${SHAKE_ANIMATION_TIMEOUT / 1000}s`;
+    this._taskComponent.getElement().style.animation = `shake ${SHAKE_ANIMATION_TIMEOUT / 1000}s`;
+
+    setTimeout(() => {
+      this._taskEditComponent.getElement().style.animation = ``;
+      this._taskComponent.getElement().style.animation = ``;
+    }, SHAKE_ANIMATION_TIMEOUT);
   }
 
   _replaceEditToTask() {
